78558bae9189fa8e130caaa06c229fc7d4ab6194
Rohan Dmello <dmellorohan@hotmail.com>
2013-10-12 13:33:55 -0700

    New file added to check out few of Git tricks

---
+++ src/main/java/com/rohan/mywebapp/MyApp.java
@@ -0,0 +1,14 @@
+package com.rohan.mywebapp;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rohan.Dmello
+ * Date: 10/12/13
+ * Time: 1:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyApp {
+    public static void main(String [] args){
+        System.out.println("Hello");
+    }
+}
